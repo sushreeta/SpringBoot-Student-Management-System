a00fd178ab09b8d4e90921ad0b7f37e8b6d5f251
Sushreeta Sahoo <sushreeta04@gmail.com>
2021-12-30 18:49:54 +0530

    feat(sms): update student

--- src/main/java/com/sushreeta/studentmanagementsystem/controller/StudentController.java
+++ src/main/java/com/sushreeta/studentmanagementsystem/controller/StudentController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -35,6 +36,25 @@ public class StudentController {
     @PostMapping("/students")
     public String saveStudent(@ModelAttribute("student") Student student){
         studentService.saveStudent(student);
+        return "redirect:/students";
+    }
+
+    @GetMapping("/student/edit/{id}")
+    public String editStudentForm(@PathVariable Long id, Model model){
+        model.addAttribute("student", studentService.getStudentById(id));
+        return "edit_student";
+    }
+
+    @PostMapping("/students/{id}")
+    public String updateStudent(@PathVariable Long id, @ModelAttribute("student") Student student, Model model){
+//        get student from database by Id
+        Student existingStudent = studentService.getStudentById(id);
+        existingStudent.setFirstName(student.getFirstName());
+        existingStudent.setLastName(student.getLastName());
+        existingStudent.setEmail(student.getEmail());
+
+//        save updated student object
+        studentService.updateStudent(existingStudent);
         return "redirect:/students";
     }
 }
--- src/main/java/com/sushreeta/studentmanagementsystem/service/StudentService.java
+++ src/main/java/com/sushreeta/studentmanagementsystem/service/StudentService.java
@@ -8,4 +8,6 @@ public interface StudentService {
 
     List<Student> getAllStudents();
     Student saveStudent(Student student);
+    Student updateStudent(Student student);
+    Student getStudentById(Long id);
 }
